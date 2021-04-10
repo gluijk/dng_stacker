bd6057c98c4f9ea8be32aac5eaa93c3262af2ee1
Guillermo Luijk <45771557+gluijk@users.noreply.github.com>
2021-04-11 01:43:50 +0200

    Add files via upload

--- myversion/hdr_DNG.R
+++ myversion/hdr_DNG.R
@@ -21,7 +21,7 @@ img=list()
 txt=list()
 for (i in 1:N) {
     img[[i]]=readTIFF(paste0(NAME, i, ".tiff"), native=F, convert=F)
-    txt[[i]]=paste0(NAME, i, ".tiff_", NAME, i+1, ".tiff")
+    txt[[i]]=paste0(NAME, i, "vs", NAME, i+1)
 }
 
 
@@ -34,9 +34,9 @@ exprel=list()
 f=array(-1, N-1)
 for (i in 1:(N-1)) {
     indices[[i]]=which(img[[i]]>=MIN   & img[[i]]<=MAX &
-                       img[[i+1]]>=MIN & img[[i+1]]<=MAX)
+                           img[[i+1]]>=MIN & img[[i+1]]<=MAX)
     exprel[[i]]=img[[i+1]][indices[[i]]]/img[[i]][indices[[i]]]
-    f[i]=median(exprel[[i]])  # linear exposure correction factor
+    f[i]=median(exprel[[i]])  # Slinear exposure correction factor
 }
 print("Relative exposures (EV):")
 print(round(log(cumprod(f),2),2))
@@ -45,15 +45,18 @@ print(round(log(cumprod(f),2),2))
 png("relexposure_histogram.png", width=640, height=320*(N-1))
 par(mfrow=c(N-1,1))
 for (i in 1:(N-1)) {
-    hist(exprel[[i]][exprel[[i]]>=f[i]*0.75 & exprel[[i]]<=f[i]*1.25],
+    flog=log(f[i],2)
+    rflog=round(flog)
+    exprelog=log(exprel[[i]],2)
+    hist(exprelog[exprelog>=rflog-1/3 & exprelog<=rflog+1/3],
          main=paste0('Relative exposure histogram (', txt[[i]], ')'),
-         xlab='Linear relative exposure',
-         breaks=seq(f[i]*0.75, f[i]*1.25, length.out=800)
+         xlab=paste0('EV (calculated: ', round(flog,2), 'EV)'),
+         breaks=seq(rflog-1/3, rflog+1/3, length.out=800)
     )
-    abline(v=f[i], col='red')
-    abline(v=round(f[i]), col='gray', lty='dotted')  # closest int EV mark
+    abline(v=flog, col='red')  # calculated relative exposure
+    abline(v=rflog, col='gray', lty='dotted')  # closest int EV mark
 }
-dev.off()  
+dev.off() 
 
 # Relative exposure calculation map
 solape=array(-1, N-1)
@@ -73,18 +76,16 @@ hdr=img[[1]]  # start with lowest exposure
 mapafusion=img[[i]]*0+1
 for (i in 2:N) {
     indices=which(img[[i]]<=MAX)  # non-clipped highest exposure
-    hdr[indices]=img[[i]][indices]/cumprod(f)[i-1]  # overwrite replicating exp
+    hdr[indices]=img[[i]][indices]/cumprod(f)[i-1]  # overwrite+exp correction
     mapafusion[indices]=i
 }
-if (max(hdr)<1) print(paste0("Output data will be ETTR'ed by: +",
+if (max(hdr)<1) print(paste0("Output ETTR'ed by: +",
                              round(-log(max(hdr),2),2), "EV"))
-writeTIFF((hdr/max(hdr))^(1/gamma), "hdr.tif", bits.per.sample=16, compression="none")
+writeTIFF((hdr/max(hdr))^(1/gamma), "hdr.tif", bits.per.sample=16,
+          compression="none")
 
 # Fusion map and RAW data files contributions
 writeTIFF((mapafusion-1)/(N-1), "mapafusion.tif",
           bits.per.sample=8, compression="LZW")
 for (i in 1:N) print(paste0("Contribution of ", NAME, i, ".tiff: ",
             round(length(which(mapafusion==i))/length(mapafusion)*100,2),"%"))
-hist(mapafusion,
-     main='Contribution of RAW data files',
-     xlab=paste0('Source RAW file (1..', N, ')'))
